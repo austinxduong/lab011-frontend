97c9f4e34b67887cadeb041a2e1003ee335302b5
austinxduong <axduong2@gmail.com>
2021-05-10 17:50:42 -0700

    call api function, pass in id, set state

---
+++ src/cats/CatItem.js
@@ -0,0 +1,23 @@
+import { Link } from 'react-router-dom';
+import React, { Component } from 'react';
+import './CatItem.css';
+
+class CatItem extends Component {
+
+  render() {
+    const cat = this.props.cat;
+
+    return (
+      <li className="CatItem">
+        <Link to={ `/cats/${cat.id}`}>
+          <h2>{cat.name}</h2>
+          <img src={cat.url} alt ={cat.name}/>
+          <p>lives: {cat.lives}</p>
+        </Link>        
+      </li>
+    );
+  }
+
+}
+
+export default CatItem;
